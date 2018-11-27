6cba52d719e472e4e3826bae17c3e0ed73c5bc0e
akhilNandanavanam <n.saiakhil39@gmail.com>
2018-11-27 12:55:11 +0530

    Added HeroService

--- TourOfHeroes/src/app/heroes/heroes.component.ts
+++ TourOfHeroes/src/app/heroes/heroes.component.ts
@@ -1,21 +1,25 @@
+import { HeroService } from '../hero.service';
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-  hero =HEROES;
+  heroes : Hero[];
   selectedHero : Hero;
-    constructor() { }
+    constructor(private heroService : HeroService) { }
 
   ngOnInit() {
-  }
+    this.heroes = this.heroService.getHero();
+    
+    }
   onSelect(hero : Hero):void{
     this.selectedHero = hero;
   }
-
+  
 }
