cf58bcbde9011334c16f6539b7444f8153b007b2
akhilNandanavanam <n.saiakhil39@gmail.com>
2018-11-27 18:54:52 +0530

    Added Routing

--- TourOfHeroes/src/app/app-routing.module.ts
+++ TourOfHeroes/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule, Route } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 import { DashComponent } from './dash/dash.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
@@ -10,6 +11,9 @@ const routes: Routes = [
     component:  DashComponent
 
   },
+  { 
+    path: 'detail/:id', 
+    component: HeroDetailComponent },
   {
     path: 'heroes',
     component : HeroesComponent
